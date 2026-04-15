221684a488f5b2e1dd363401d5a428c2f1ddb8d4
yaraalhammouri <179583736+yaraalhammouri@users.noreply.github.com>
2026-04-16 03:30:56 +0400

    added live demo link

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
         "Designed and developed a responsive portfolio website using React. Features include smooth navigation, modular component structure, and a clean UI for showcasing skills and projects.",
       tech: "React, CSS, JavaScript",
       github: "https://github.com/yaraalhammouri/FUTURE_FS_01",
-      live: "https://your-live-link.com",
+      live: "future-fs-01-ruby-rho.vercel.app",
     },
    
    
